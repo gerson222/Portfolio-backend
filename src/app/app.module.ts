c09383375e824561d379515415a2fc2c3454a464
Gica <gquirozpretell@frba.utn.edu.ar>
2022-12-28 17:22:06 -0300

    ahora si anda angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PerfilComponent } from './componentes/perfil/perfil.component';
+import { ExperienciasComponent } from './componentes/experiencias/experiencias.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { SkillsComponent } from './componentes/skills/skills.component';
+import { ProyectoComponent } from './componentes/proyecto/proyecto.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { RegistroComponent } from './componentes/registro/registro.component';
+import { EdicionComponent } from './componentes/edicion/edicion.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PerfilComponent,
+    ExperienciasComponent,
+    EducacionComponent,
+    SkillsComponent,
+    ProyectoComponent,
+    LoginComponent,
+    RegistroComponent,
+    EdicionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
